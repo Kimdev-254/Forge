0e7545085d641bb52d8934742710b8cf5b0c6b66
Kimdev254 <bonifacekimani715@gmail.com>
2024-12-20 22:56:05 +0300

    site config

---
+++ src/config/site.ts
@@ -0,0 +1,21 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Forge",
+  description:
+    "Transform your habits, build consistency, and achieve your goals with our powerful discipline-building platform.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Dashboard",
+      href: "/dashboard",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/forge",
+    github: "https://github.com/forge",
+  },
+}
