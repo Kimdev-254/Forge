97a77d603e2b3f4c9290e99d8dae5b2d7ac17779
Kimdev254 <bonifacekimani715@gmail.com>
2024-12-25 01:23:46 +0300

    switching from Mono snas to Inter

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,6 +1,6 @@
-import { Mona_Sans as FontSans } from "next/font/google"
+import { Inter } from "next/font/google"
 
-export const fontSans = FontSans({
+export const fontSans = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
 })
